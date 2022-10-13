3d78ea59fe39c8886c853b07a794e8755f30b584
mnithilan <115585169+mnithilan@users.noreply.github.com>
2022-10-13 17:58:53 +0530

    Create 9. Right most digit.c

---
+++ 9. Right most digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("Register number: RA2211042010013\n");
+    int num1;
+    int rightdigit;
+    printf("enter the number: ");
+    scanf("%d",&num1);
+    rightdigit = num1%10;
+    printf("\nthe right most digit is: %d",rightdigit);
+}
