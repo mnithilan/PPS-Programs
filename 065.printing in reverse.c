f5e734709f614f2eedce9a57a608be1b458f023a
mnithilan <115585169+mnithilan@users.noreply.github.com>
2022-11-25 00:33:31 +0530

    Create 065.printing in reverse.c

---
+++ 065.printing in reverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>  
+int main()    
+{    
+    printf("Register number: RA2211042010013\n\n");
+int n, reverse=0, reminder;    
+printf("Enter a number: ");    
+  scanf("%d", &n);    
+  while(n!=0)    
+  {    
+     reminder=n%10;    
+     reverse=reverse*10+reminder;    
+     n/=10;    
+  }    
+  printf("Reversed Number: %d",reverse);    
+return 0;  
+} 
