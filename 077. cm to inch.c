e3536d2c96967d9726452a17e066c5a3a4d1cb37
mnithilan <115585169+mnithilan@users.noreply.github.com>
2022-11-25 01:16:48 +0530

    Create 077. cm to inch.c

---
+++ 077. cm to inch.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()  
+{  
+    float  inch,cm;  
+
+    printf("Enter length in centimeter:");  
+    scanf("%f", &cm);  
+
+    inch=cm*2.54;  
+
+    printf("length in inch is %.2f\n",inch);  
+
+    return 0;  
+}  
