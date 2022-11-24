67aa4a4de91f7c86497f134db7fcd007b7c1d908
mnithilan <115585169+mnithilan@users.noreply.github.com>
2022-11-25 01:52:56 +0530

    Update and rename 076. meter to millimeter.c to 076. metre to millimetre.c

--- 076. metre to millimetre.c
+++ 076. metre to millimetre.c
@@ -2,14 +2,14 @@ int main()
 {  
     float  mm, m;  
 
-    printf("Enter distance in meter:");  
+    printf("Enter distance in metre:");  
     scanf("%f", &m);  
 
 
     mm = m * 100.0;  
 
 
-    printf("Distance in Milimeter is %.2f\n", mm);  
+    printf("Distance in Milimetre is %.2f\n", mm);  
 
     return 0;  
 } 
