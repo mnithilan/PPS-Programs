058d963807bbace3d9b55dbea9d468a2f9449a55
mnithilan <115585169+mnithilan@users.noreply.github.com>
2022-11-25 01:56:08 +0530

    Create 095.metre to feet.c

---
+++ 095.metre to feet.c
@@ -0,0 +1,14 @@
+int main() {
+
+  float metre;
+  float feet;
+
+	printf(" Program to convert Metre to Feet:  ");
+
+	printf("\n\n Enter Length in Metre  : ");
+	scanf("%f", &metre);
+
+    feet = (metre*3.26);
+
+    printf("\n\n %f metre in feet = %f  \n",metre,feet);
+}
