fb1efe3861365d4581095cf56f1b4db7cc74266d
mnithilan <115585169+mnithilan@users.noreply.github.com>
2022-11-24 22:16:09 +0530

    Create 044.else if.c

---
+++ 044.else if.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+printf("Register number: RA2211042010013\n");
+int time = 22;
+if (time < 10)
+{
+  printf("Good morning.");
+}
+else if (time < 20)
+{
+  printf("Good day.");
+} 
+else 
+{
+  printf("Good evening.");
+}
+}
