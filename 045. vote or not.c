5f839cbfe75cf8a6c91ac38ba735f5c138262225
mnithilan <115585169+mnithilan@users.noreply.github.com>
2022-11-24 22:18:07 +0530

    Create 045. vote or not.c

---
+++ 045. vote or not.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+ printf("Register number: RA2211042010013\n");
+ int age;
+ printf("enter your age\n");
+ scanf("%d",&age);
+ if(age >=18)
+ {
+     printf("can vote");
+ }
+ else
+ {
+     printf("cannot vote");
+ }
+  
+}
