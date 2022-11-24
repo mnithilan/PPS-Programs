832bbc44f858bda9e7c5d2e9146e514b24b6b9cc
mnithilan <115585169+mnithilan@users.noreply.github.com>
2022-11-24 22:14:19 +0530

    Create 043.sizeof.c

---
+++ 043.sizeof.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+ printf("Register number: RA2211042010013\n");
+ int myInt;
+ float myFloat;
+ double myDouble;
+ char myChar;
+ 
+ printf("%lu\n", sizeof(myInt));
+ printf("%lu\n", sizeof(myFloat));
+ printf("%lu\n", sizeof(myDouble));
+ printf("%lu\n", sizeof(myChar));
+  
+  return 0;
+}
