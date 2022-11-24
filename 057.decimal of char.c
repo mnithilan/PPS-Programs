2aa0cb72cd0d3200b38dec3dc0b27db0a6c3d144
mnithilan <115585169+mnithilan@users.noreply.github.com>
2022-11-25 00:16:45 +0530

    Create 057.decimal of char.c

---
+++ 057.decimal of char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Register number: RA2211042010013\n");
+    char A ='A';
+    char B ='B';
+    char C ='C';
+    printf("ASCII for char 'A' is: %d\n",A);
+    printf("ASCII for char 'B' is: %d\n",B);
+    printf("ASCII for char 'C' is: %d\n",C);
+    return 0;
+}
